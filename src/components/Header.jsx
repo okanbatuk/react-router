d16167d76c18c3bc17db39de15985bb2b89b4025
myrn <okanbatuk@protonmail.com>
2023-06-04 23:33:50 +0300

    Improved header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,20 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Header = ({ title, navigate }) => {
   return (
     <header className="Header">
-      <h2 style={{ cursor: "pointer" }} onClick={() => navigate("/")}>
-        {title}
+      <h2>
+        <Link
+          to={"/"}
+          style={{
+            cursor: "pointer",
+            color: "inherit",
+            textDecoration: "none",
+          }}
+        >
+          {title}
+        </Link>
       </h2>
     </header>
   );
